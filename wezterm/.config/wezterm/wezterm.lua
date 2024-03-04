2be7d417ed8dab5b4b63e63c3e8a213951af689d
Luís Eduardo Batista <luiseduardob303@gmail.com>
2024-03-04 09:57:21 -0300

    Feat: Added multiplexing cfg to wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,26 +1,57 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This table will hold the configuration.
+local act = wezterm.action
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-
 config.window_decorations = "RESIZE"
-config.color_scheme = "rose-pine"
+config.color_scheme = "Gruvbox Dark (Gogh)"
 config.font = wezterm.font("JetBrainsMono Nerd Font")
--- config.window_background_opacity = 0.75
 config.show_tabs_in_tab_bar = false
 config.use_fancy_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
 config.enable_tab_bar = false
--- and finally, return the configuration to wezterm
+config.max_fps = 165
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.colors = {}
+config.keys = {
+	{
+		mods = "LEADER",
+		key = "v",
+		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "h",
+		mods = "LEADER",
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "l",
+		mods = "CTRL|SHIFT",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "h",
+		mods = "CTRL|SHIFT",
+		action = act.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "k",
+		mods = "CTRL|SHIFT",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "j",
+		mods = "CTRL|SHIFT",
+		action = act.ActivatePaneDirection("Down"),
+	},
+	{
+		mods = "CTRL|SHIFT",
+		key = "m",
+		action = act.TogglePaneZoomState,
+	},
+}
+
 return config
